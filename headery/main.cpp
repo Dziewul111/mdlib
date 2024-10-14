50599066f4afc4473e1838a11734411f180262fa
Mateusz Dziewulski <matdzie09@icloud.com>
2024-10-14 10:55:01 +0200

    fix

--- headery/main.cpp
+++ headery/main.cpp
@@ -8,13 +8,13 @@ typedef long long LL;  // XIII LO Szczecin
 #define nd second
 #define pb push_back
 template<class A, class B> auto& operator<<(ostream &o, pair<A, B> p) {
-	return o << '(' << p.st << ", " << p.nd << ')';
+		return o << '(' << p.st << ", " << p.nd << ')';
 }
 template<class T> auto operator<<(ostream &o, T x) -> decltype(x.end(), o) {
-	o << '{'; int i = 0; for(auto e : x) o << (", ")+2*!i++ << e; return o << '}';
+		o << '{'; int i = 0; for(auto e : x) o << (", ")+2*!i++ << e; return o << '}';
 }
 #define LOG(x...) cerr << "[" #x "]: ", [](auto... $) {((cerr << $ << "; "), ...); }(x), cerr << '\n'
 
 int main(){
-  cin.tie(0)->sync_with_stdio(0);
+  	cin.tie(0)->sync_with_stdio(0);
 }
