b859ce7cfdc8d0283ce1f41e0b0a6d052740bc95
Mateusz Dziewulski <matdzie09@icloud.com>
2024-10-16 23:51:23 +0200

    dodano dijkstre

--- grafy/dijkstra.cpp
+++ grafy/dijkstra.cpp
@@ -1,1 +1,34 @@
-// to do
+#include<bits/stdc++.h>
+using namespace std;
+#define st first
+#define nd second
+#define ssize(x) x.size()
+#define inf INT_MAX
+typedef long long LL;
+
+vector<int> dijkstra(vector<vector<pair<int, LL>>>& adj, int u){
+	int n = ssize(adj);
+
+	priority_queue<pair<LL, int>> q;
+	vector<int> ret(n, inf);
+	vector<bool> odw(n, 0);
+	q.push({0, u});
+
+	while(ssize(q)){
+		LL w = q.top().nd;
+		q.pop();
+
+		if(odw[w])
+			continue;
+
+		odw[w] = 1;
+
+		for(auto i : adj[w]){
+			if(ret[i.st] > ret[w] + i.nd){
+				ret[i.st] = ret[w] + i.nd;
+				q.push({-ret[i.st], i.st});
+			}
+		}
+	}
+	return ret;
+}
