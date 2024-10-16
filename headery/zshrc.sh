0bbb0d7588b82a8d91eb148ec558e20cc0bb0815
Mateusz Dziewulski <matdzie09@icloud.com>
2024-10-17 00:16:30 +0200

    aktualizacja headerów

--- headery/zshrc.sh
+++ headery/zshrc.sh
@@ -1,8 +1,8 @@
 
 xd(){
-  g++-14 -Wall -Wshadow -Wextra -Wfloat-equal -std=c++20 -o $1 -O3 $1.cpp -D_GLIBCXX_DEBUG
+  	g++-14 -Wall -Wshadow -Wextra -Wfloat-equal -Wfatal-errors -Wconversion -std=c++20 -o $1 -O3 $1.cpp -D_GLIBCXX_DEBUG
 }
 
 xdd(){
-	g++-14 -Wall -Wshadow -Wextra -Wfloat-equal -std=c++20 -0 $1 -O3 $1.cpp -D_GLIBCXX_DEBUG -DDEBUG
+	g++-14 -Wall -Wshadow -Wextra -Wfloat-equal -Wfatal-errors -Wconversion -std=c++20 -0 $1 -O3 $1.cpp -D_GLIBCXX_DEBUG -DDEBUG
 }
