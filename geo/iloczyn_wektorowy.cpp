def8b58499b18bc8af1c5b482b0b117ddeffaefb
Mateusz Dziewulski <matdzie09@icloud.com>
2024-10-18 23:21:29 +0200

    fix

--- geo/iloczyn_wektorowy.cpp
+++ geo/iloczyn_wektorowy.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define pair<int, int> pii
 
 int cross(pii a, pii b, pii c){
-	return (b.nd - a,nd) * (c.st - b.st) - (c.nd - b.nd) * (b.st - a.st);
+	return (b.nd - a.nd) * (c.st - b.st) - (c.nd - b.nd) * (b.st - a.st);
 	// 0    = linia prosta    | forward    | geradeaus
 	// 0 >  = skręt w prawo   | turn left  | nach links
 	// 0 <  = skręt w lewo	  | turn right | nach rechts	
