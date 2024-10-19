eab0860561486467ebb596c927367d5bc17cb44a
Mateusz Dziewulski <matdzie09@icloud.com>
2024-10-19 22:39:51 +0200

    dodano unionfind

---
+++ struktury-danych/union_find.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+
+Union find 
+
+dla elementów ∈ [0, n)
+
+*/
+
+
+
+struct uinon_find{
+	vector<int> link;
+	vector<int> size;
+
+	uinon_find(int n){
+		link.resize(n);
+		size.resize(n, 1);
+
+		for(int i = 0; i < n; i++)
+			link[i] = i;
+	}
+
+	int root(int x){
+		if(link[x] == x)
+			return x;
+		return link[x] = root(link[x]);
+	}
+
+	bool find(int a, int b){
+		return root(a) == root(b);
+	}
+
+	void unite(int a, int b){
+		a = root(a);
+		b = root(b);
+
+		if(a != b){
+			if(size[a] < size[b])
+				swap(a, b);
+
+			link[b] = a;
+			size[a] += size[b];
+		}
+	}
+};
