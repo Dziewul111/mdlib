243793af4882ee46520692ade1cbc1d42ed0778b
Mateusz Dziewulski <matdzie09@icloud.com>
2024-10-22 07:31:32 +0200

    fix

--- matma/mat-mul.cpp
+++ matma/mat-mul.cpp
@@ -3,20 +3,20 @@ using namespace std;
 #define matrix vector<vector<int>> 
 #define ssize(x) x.size()
 
-matrix matmul(matrix a, matrix b){
-	assert(ssize(a) == ssize(b[0]));
-	assert(ssize(a[0]) == ssize(b));
+matrix matmul(matrix m1, matrix m2){
+	assert(ssize(m1) == ssize(m2[0]));
+	assert(ssize(m1[0]) == ssize(m2));
 
-	int a = ssize(a);
-	int b = ssize(a[0]);
-	int d = ssize(b[0]);
+	int a = (int)ssize(m1);
+	int b = (int)ssize(m1[0]);
+	int d = (int)ssize(m2[0]);
 
 	matrix ret(a, vector<int>(d, 0));
 
 	for(int i = 0; i < a; i++)
 		for(int j = 0; j < d; j++)
 			for(int k = 0; k < b; k++)
-				ret[i][j] += a[i][k] * b[k][j];
+				ret[i][j] += m1[i][k] * m2[k][j];
 
 	return ret;
 }
