de241695a1f7a7c94b6107262a8467036faba7ce
Mateusz Dziewulski <matdzie09@icloud.com>
2025-01-21 16:11:16 +0100

    rho fix

--- matma/rho.cpp
+++ matma/rho.cpp
@@ -9,14 +9,15 @@ LL llmul(LL a, LL b, LL mod){
 }
 
 LL rho(LL n){
-	for(LL c = 1;; c++){
-		auto f = [&](LL x){return (llmul(x, x, n) + c) % mod;};
+	if(n % 2 == 0) return 2;
+	for(int c = 1;; c++){
+		auto f = [&](LL x){return (llmul(x, x, n) + c) % n;};
 
-		int x = 2, y = f(x), p;
+		LL x = 2, y = f(x), p;
 
 		while((p = gcd(abs(x - y), n)) == 1)
-			x = f(x), y = f(f(x));
-		if(p != n)
-			return p;
+			x = f(x), y = f(f(y));
+
+		if(p != n) return p;
 	}
 }
